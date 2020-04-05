6dc77a4b6ae53ce95e33bd1e34726848dbdf4c90
Benoit Podwinski <me@benoitpodwinski.com>
2020-04-05 16:13:11 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                     { loader: "css-loader" },
                     { loader: "sass-loader" }
                 ]
-            },
+            }
         ]
     },
     plugins: [
