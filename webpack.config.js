b73133e427c6084f7e7af9b02ded09de7be9040d
Benoit Podwinski <me@benoitpodwinski.com>
2020-05-12 19:10:37 +0200

    Mega update

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,85 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let sourceMap = true;
+let outputStyle = "expanded";
 
-if (process.env.NODE_ENV === 'production') {
-    sourceMap = false;
+if (process.env.NODE_ENV === "production") {
+  sourceMap = false;
+  outputStyle = "compressed";
 }
 
 module.exports = {
-    entry: {
-        main: [
-            './sass/style.scss'
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, './'),
-        filename: 'webpack.log'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader" }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        }),
-    ]
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    main: ["./assets/js/app.js", "./assets/scss/main.scss"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./"),
+    filename: "assets/js/main.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "assets/css/[name].css",
+      chunkFilename: "assets/css/[id].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./assets/fonts/",
+              publicPath: "../fonts/",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./assets/img/",
+              publicPath: "../img/",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: sourceMap,
+              url: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: sourceMap,
+              sassOptions: {
+                outputStyle: outputStyle,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
